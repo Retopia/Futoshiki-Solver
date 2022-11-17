7a64e18294b7d6dbbf25c55d301f070591601cc1
Retopia <prestonleetang0123@gmail.com>
2022-11-17 14:50:57 -0500

    Fixed bugs in parse_file()

--- main.py
+++ main.py
@@ -1,42 +1,60 @@
 import argparse
 
-initial_state = [[0] * 5 for x in range(5)]
-horizontal_conditions = [[0] * 5 for x in range(4)]
-vertical_conditions = [[0] * 4 for x in range(5)]
+initial_state = [["0"] * 5 for x in range(5)]
+horizontal_conditions = [["0"] * 4 for x in range(5)]
+vertical_conditions = [["0"] * 5 for x in range(4)]
 input_file = ""
 
 # Reads an input text file
 # Stores data in relevant global variables
-# WIP (Untested for now)
 def parse_input(file_name):
     data = ""
     # Reads the entire file into data
     with open(file_name) as file:
         data = file.read()
 
+    # Remove all trailing spaces from data
+    temp_data = []
+    for i in range(len(data.split("\n"))):
+        temp_data.append(data.split("\n")[i].rstrip())
+    data = "\n".join(temp_data)
+
     # Should contain the 3 sections of an input file
     parts = data.split("\n\n")
+
+    # for p in parts:
+    #     print(p, "\n")
 
     # Parse first section and put it in initial_state
     global initial_state
     board_row = parts[0].split("\n")
-    for r in range(len(board_row)):
-        for c in range(len(board_row[r].split(" "))):
-            initial_state[r][c] = board_row[r][c]
+    for r in range(5):
+        board_col = board_row[r].split(" ")
+        for c in range(5):
+            initial_state[r][c] = board_col[c]
+
+    # print('\n'.join([' '.join([str(cell) for cell in row]) for row in initial_state]))
+    # print()
 
     # Parse second section and put it in horizontal_conditions
     global horizontal_conditions
-    hc_rows = parts[0].split("\n")
-    for r in range(len(hc_rows)):
-        for c in range(len(hc_rows[r].split(" "))):
-            horizontal_conditions[r][c] = hc_rows[r][c]
+    hc_row = parts[1].split("\n")
+    for r in range(5):
+        hc_col = hc_row[r].split(" ")
+        for c in range(4):
+            horizontal_conditions[r][c] = hc_col[c]
+
+    # print('\n'.join([' '.join([str(cell) for cell in row]) for row in horizontal_conditions]))
+    # print()
 
     # Parse third section and put it in vertical_conditions
     global vertical_conditions
-    vc_rows = parts[0].split("\n")
-    for r in range(len(vc_rows)):
-        for c in range(len(vc_rows[r].split(" "))):
-            vertical_conditions[r][c] = vc_rows[r][c]
+    vc_row = parts[2].split("\n")
+    for r in range(4):
+        vc_col = vc_row[r].split(" ")
+        for c in range(5):
+            vertical_conditions[r][c] = vc_col[c]
+    # print('\n'.join([' '.join([str(cell) for cell in row]) for row in vertical_conditions]))
 
 
 def main():
