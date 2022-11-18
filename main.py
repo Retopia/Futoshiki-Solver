21a852f3f54d8e8005f84649a537b24a75a47bca
majikyouser <sahiagb@gmail.com>
2022-11-17 23:42:51 -0500

    Moved algo to main

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
+'''
+Project 2:
+Futoshiki Solver
+Steven Han and Preston Tang
+CS 4613
+'''
+
+
 import argparse
 
 initial_state = [["0"] * 5 for x in range(5)]
 horizontal_conditions = [["0"] * 4 for x in range(5)]
 vertical_conditions = [["0"] * 5 for x in range(4)]
 input_file = ""
+
+
+def isValid(board, row, col, num):
+    pass
+
+def solveBoardHelper(board, col, row):
+    pass
+
+def createOutput():
+    pass
+
 
 # Reads an input text file
 # Stores data in relevant global variables
@@ -69,5 +88,56 @@ def main():
 
     parse_input(input_file)
 
+    # start the algorithm
+    solveBoardHelper(board, 0, 0)
+
 if __name__ == "__main__":
     main()
+
+''' Steven's Sudoku Solver in C++ for reference
+class Solution {
+public:
+    bool isValid(vector<vector<char>>& board, int row, int col, char num)
+    {
+        for(int i = 0; i < 9; i++)
+        {
+            if(board[i][col] == num)
+                return false;
+            if(board[row][i] == num)
+                return false;
+            if(board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == num)
+                return false;
+        }
+        return true;
+    }
+    
+    bool solveSudokuHelp(vector<vector<char>>& board, int col, int row)
+    {
+        if(col == 9)
+        {
+            row += 1;
+            col = 0;
+        }
+        if(row == 9)
+            return true;
+        if(board[row][col] == '.')
+        {
+            for(char i = '1'; i <= '9'; i++)
+            {
+                if(isValid(board, row, col, i))
+                {
+                    board[row][col] = i;
+                    if (solveSudokuHelp(board, col + 1, row))
+                        return true;
+                }
+                board[row][col] = '.';
+            }
+            return false;
+        }
+        return solveSudokuHelp(board, col + 1, row);
+    }
+    void solveSudoku(vector<vector<char>>& board) {
+        solveSudokuHelp(board, 0, 0);
+    }
+};
+'''
